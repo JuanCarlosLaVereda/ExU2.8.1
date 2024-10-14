554d9170c3f026a25addfd1a5a5674b77cc2831a
JuanCarlosLaVereda <juasanper2@alu.edu.gva.es>
2024-10-14 16:27:07 +0200

    PUTS Y POSTS terminados

--- android/app/src/main/java/com/example/exu2_8/ExU2.8.1.dart
+++ android/app/src/main/java/com/example/exu2_8/ExU2.8.1.dart
@@ -8,6 +8,7 @@ void main(List<String> args) async{
   String todos = "https://jsonplaceholder.typicode.com/todos";
   String espciador = "********************************************************************************************************";
 
+/*  //getters
   try{
     await GET(posts);
     print(espciador);
@@ -16,7 +17,31 @@ void main(List<String> args) async{
     //await GET(todos);
   } catch (e){
     print(e);
-  }
+  }*/
+
+/*  //posts
+  try{
+    await POSTposts(posts);
+    print(espciador);
+    await POSTtodos(todos);
+    print(espciador);
+    await POSTphotos(photos);
+    print(espaciador);
+  } catch (e){
+    print(e);
+  }*/
+
+/*  //puts
+  try{
+    await PUTposts(posts);
+    print(espciador);
+    await PUTtodos(todos);
+    print(espciador);
+    await PUTphotos(photos);
+    print(espciador);
+  } catch(e){
+    print(e);
+  }*/
 
 
 }
@@ -42,5 +67,105 @@ POSTposts(String link) async{
   'application/json'};
 
   String body = convert.jsonEncode(mapBody);
+
+  var respuesta = await http.post(url, headers: headers, body: body);
+
+  if (respuesta.statusCode == 200 || respuesta.statusCode == 201){
+    print(respuesta.body);
+  } else{
+    print("Something went wrong, code error: ${respuesta.statusCode}");
+  }
+}
+
+POSTtodos(String link) async{
+  var url = Uri.parse(link);
+
+  Map mapBody = {"userId": 1, "id": 333, "tittle": "Que numerin", "completed": "true"};
+
+  Map<String, String> headers = {'Content-Type':
+  'application/json'};
+
+  String body = convert.jsonEncode(mapBody);
+
+  var respuesta = await http.post(url, headers: headers, body: body);
+
+  if (respuesta.statusCode == 200 || respuesta.statusCode == 201){
+    print(respuesta.body);
+  } else{
+    print("Something went wrong, code error: ${respuesta.statusCode}");
+  }
+}
+
+POSTphotos(String link) async{
+  var url = Uri.parse(link);
+
+  Map mapBody = {"albumId": 1, "id": 333, "tittle": "Que numerin", "url": "url", "thumbnailUrl": "thumbnailUrl"};
+
+  Map<String, String> headers = {'Content-Type':
+  'application/json'};
+
+  String body = convert.jsonEncode(mapBody);
+
+  var respuesta = await http.post(url, headers: headers, body: body);
+
+  if (respuesta.statusCode == 200 || respuesta.statusCode == 201){
+    print(respuesta.body);
+  } else{
+    print("Something went wrong, code error: ${respuesta.statusCode}");
+  }
+}
+
+PUTposts(String link) async{
+  var url = Uri.parse(link);
+
+  Map mapBody = {"userId": 1, "id": 1, "tittle": "Que numerin", "completed": "true"};
+  Map<String, String> headers = {'Content-Type':
+  'application/json'};
+
+  String body = convert.jsonEncode(mapBody);
+
+  var respuesta = await http.put(url, headers: headers, body: body);
+
+  if(respuesta.statusCode == 200 || respuesta.statusCode == 201){
+    print(respuesta.body);
+  } else{
+    print("Something went wrong, code error: ${respuesta.statusCode}");
+  }
+}
+
+PUTtodos(String link) async{
+  var url = Uri.parse(link);
+  Map mapBody = {"userId": 1, "id": 1, "tittle": "Que numerin", "body": "aut et excepturi dicta laudantium sint rerum nihil\nlaudantium et at\na neque minima officia et similique libero et\ncommodi voluptate qui"};
+
+  Map<String, String> headers = {'Content-Type':
+  'application/json'};
+
+  String body = convert.jsonEncode(mapBody);
+
+  var respuesta = await http.put(url, headers: headers, body: body);
+
+  if(respuesta.statusCode == 200 || respuesta.statusCode == 201){
+    print(respuesta.body);
+  } else{
+    print("Something went wrong, code error: ${respuesta.statusCode}");
+  }
+}
+
+PUTphotos(String link) async{
+  var url = Uri.parse(link);
+  Map mapBody = {"albumId": 1, "id": 1, "tittle": "Que numerin", "url": "url", "thumbnailUrl": "thumbnailUrl"};
+
+  Map<String, String> headers = {'Content-Type':
+  'application/json'};
+
+  String body = convert.jsonEncode(mapBody);
+
+  var respuesta = await http.put(url, headers: headers, body: body);
+
+  if(respuesta.statusCode == 200 || respuesta.statusCode == 201){
+    print(respuesta.body);
+  } else{
+    print("Something went wrong, code error: ${respuesta.statusCode}");
+  }
 }
 
